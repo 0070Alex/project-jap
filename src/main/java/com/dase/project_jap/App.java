b2e5e1ff850459dae3fab2cc2b82a98f60544b19
0070Alex <d700.elias@gmail.com>
2023-07-05 22:55:54 -0600

    version inicial del proyecto

---
+++ src/main/java/com/dase/project_jap/App.java
@@ -0,0 +1,13 @@
+package com.dase.project_jap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
